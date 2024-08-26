14648780f498a3553a3ea8fe89fc90b5b008197b
Eric Souza Romero <eriicsouzaromeroo@gmail.com>
2024-08-26 18:16:17 -0300

    Integrated prisma

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,14 @@
 import fastify from 'fastify'
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data:{
+        name:'Clueroi',
+        email:"eriicsouzaromeroo@gmail.com"
+    },
+})
 
 
 export const app = fastify( )
