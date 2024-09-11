51d3a27b59e5c61ab768dfc8e4cd04338807cd44
unknown <eriicsouzaromeroo@gmail.com>
2024-09-11 19:33:14 -0300

    Add: Is not allowed to check-in after 20 minutes

--- src/use-cases/test/validate-check-in.spec.ts
+++ src/use-cases/test/validate-check-in.spec.ts
@@ -42,4 +42,25 @@ describe('CheckIn Use Case', () => {
             })
         ).rejects.toBeInstanceOf(ResourceNotFound)
     })
+
+    it('should not be able to validate the check-in ater 20 minutes of its creation', async()=>{
+        vi.setSystemTime(new Date(2023, 0, 1, 13, 30))
+        
+        const createdCheckIn = await inMemoryRepo.create({
+            gym_id:'Gym-01',
+            user_id:'User-01'
+        })
+
+        const twentyOneMinutesInMiliseconds = 1000 * 60 * 21
+
+        vi.advanceTimersByTime(twentyOneMinutesInMiliseconds)
+
+        const {checkIn} = await sut.execute({
+            checkInId: createdCheckIn.id
+        })
+
+        await expect(()=> sut.execute({
+            checkInId:createdCheckIn.id,
+        })).rejects.toBeInstanceOf(Error)
+    })
 })
--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -5,6 +5,8 @@ import { ResourceNotFound } from './errors/resource-not-found-error'
 import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins'
 import { MaxDistanceError } from './errors/max-distance-error'
+import dayjs from 'dayjs'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
 
 
 interface ValidateUseCaseRequest {
@@ -29,7 +31,18 @@ export class ValidateUseCase {
             throw new ResourceNotFound
         }
 
+        const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+            checkIn.created_at,
+            'minutes'
+        )
+
+        if(distanceInMinutesFromCheckInCreation > 20){
+            throw new LateCheckInValidationError()
+        }
+
         checkIn.is_validated = new Date()
+
+        await this.checkInsRepository.save(checkIn)
 
         return {
             checkIn
