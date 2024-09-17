2ed7fd3dfa78e0d0d49d1c5573f72a893169d16b
unknown <eriicsouzaromeroo@gmail.com>
2024-09-17 16:13:08 -0300

    Chore: Register test e2e

--- src/http/controller/register.spec.ts
+++ src/http/controller/register.spec.ts
@@ -1,3 +1,27 @@
-import { test } from "vitest";
+import request from "supertest";
+import { describe, expect, it, beforeAll, afterAll } from "vitest";
+import { app } from "@/app";
 
-test('ok', ()=>{})
+
+
+describe('register(e2e)', () => {
+beforeAll(async()=>{
+    await app.ready()
+})
+
+afterAll(async()=>{
+    await app.close()
+})
+
+    it('should be able to register', async () => {
+        const response = await request(app.server)
+            .post('/users')
+            .send({
+                name: 'User-01',
+                email: 'user@example.com',
+                password: '123456'
+            })
+
+        expect(response.statusCode).toEqual(201)
+    })
+})
